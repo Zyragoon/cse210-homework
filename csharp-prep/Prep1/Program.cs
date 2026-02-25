e2c6be4958b8b6b082f3a734b90d0fd46d52b400
Zyragoon <Zyragoon@gmail.com>
2026-02-25 09:35:38 -0700

    Finishing prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String  first = Console.ReadLine();
+
+        Console.Write("what is your last name?");
+        String last = Console.ReadLine();
+
+        Console.WriteLine($"your name is {last},{first},{last}.");
+
+       
+        
+
     }
 }
